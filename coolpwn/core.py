a0a1cffc69a0997e5d95c9751e0a110add12b070
kais <kaisong39@gmail.com>
2021-07-31 08:49:34 +0800

    2

---
+++ coolpwn/core.py
@@ -0,0 +1,32 @@
+import os
+
+class PwnContext:
+    def __init__(self, process, elf):
+        self.elf = elf
+        self.p = process
+        self.breaks = {}
+        self.gdbScript = ""
+
+    def debug(self,func):
+        rebase = ''
+        if self.elf.pie:
+            rebase = "$rebase"
+        addr = self.breaks[func.__name__]
+        self.gdbScript += """
+            b *{rebase}({addr})\n
+        """.format(addr=addr,rebase=rebase)
+        gdb.attach(self.p, self.gdbScript)
+
+    def addDebug(self,addr=None):
+        def debugDecorator(func):
+            if addr == None: addr = func.__name__
+            if not self.breaks: self.breaks = {}
+            self.breaks[func.__name__] = addr
+            return func
+        return debugDecorator
+    
+    def disableAlarm(self):
+        self.gdbScript += "skip function alarm\n" 
+    
+    def nopAlarm(self):
+        os.system('sed -i s/alarm/isnan/g ./' + p.argv[0])
